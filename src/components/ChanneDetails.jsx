e2ac62ab7c9b738e0e6e6d2df07f8047fecb62fd
spirit-124 <bajrangofficial580@gmail.com>
2023-03-23 19:52:27 +0530

    [Completed] project Setup

---
+++ src/components/ChanneDetails.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ChanneDetails = () => {
+  return <div>ChanneDetails</div>;
+};
+
+export default ChanneDetails;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export {default as Navbar} from './Navbar';
+export {default as ChanneDetails} from './ChanneDetails';
+export {default as Feed} from './Feed';
+export {default as SearchFeed} from './SearchFeed';
+export {default as VideoDetails} from './VideoDetails';
